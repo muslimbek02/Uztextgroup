9eceb10738ff0e9274f8e5c2ae296b693484c596
Abdumannon Shamsiyev <0605AbMu@gmail.com>
2023-02-17 19:28:43 +0500

    Created fetch service with Axios

---
+++ src/Services/FetchService.js
@@ -0,0 +1,93 @@
+import { Axios } from "axios";
+const config = require("../AppConfig/config.json");
+
+
+const axios = new Axios({
+    baseURL: config.BACKEND_API_URL
+})
+
+export default FetchService = {
+    axios: axios,
+
+    /**
+     * Get resource from API
+     * @param {string} path 
+     * @param {import("axios").AxiosDefaults} config 
+     * @returns 
+     */
+    async GetAsync(path, config = null) {
+        try {
+            return await this.ExecuteRequestAsync(path, null, 'GET', config);
+        } catch (error) {
+            throw error;
+        }
+    },
+    /**
+     * 
+     * @param {string} path 
+     * @param {object} data 
+     * @param {import("axios").AxiosDefaults} config 
+     * @returns 
+     */
+    async PostAsync(path, data, config = null) {
+        try {
+            return await this.ExecuteRequestAsync(path, data, 'POST', config)
+        } catch (error) {
+            throw error;
+        }
+    },
+    /**
+     * 
+     * @param {string} path 
+     * @param {object} data 
+     * @param {import("axios").AxiosDefaults} config 
+     * @returns 
+     */
+    async UpdateAsync(path, data, config = null) {
+        try {
+            return await this.ExecuteRequestAsync(path, data, 'PUT', config);
+        } catch (error) {
+            throw error;
+        }
+    },
+    /**
+     * 
+     * @param {string} path 
+     * @param {import("axios").AxiosDefaults} config 
+     * @returns 
+     */
+    async Delete(path, config = null) {
+        try {
+            return await this.ExecuteRequestAsync(path, null, 'DELETE', config)
+        } catch (error) {
+            throw error;
+        }
+    },
+    /**
+     * 
+     * @param {string} url 
+     * @param {object} data 
+     * @param {'POST' | 'GET' | 'PUT' | 'DELETE'} method 
+     * @param {import("axios").AxiosDefaults} config 
+     */
+    async ExecuteRequestAsync(path, data, method, config) {
+        try {
+            this.SetConfig(config);
+            return await this.axios.request({
+                url: path,
+                data: data,
+                method: method
+            })
+        } catch (error) {
+            throw error;
+        }
+    },
+    /**
+     * 
+     * @param {import("axios").AxiosDefaults} config 
+     */
+    SetConfig(config) {
+        if (config !== null)
+            this.axios.defaults = config;
+    }
+}
